f650c09cddd364b5ae0f5a1a2c0b80ed14903054
dellumiquinga4 <dellumiquinga4@espe.edu.ec>
2025-01-05 23:48:20 -0500

    GtwSeguridadProcesadorcss

--- PROYECTO/PaymentGateway/frontend/src/app/GtwSeguridadProcesador/read/page.js
+++ PROYECTO/PaymentGateway/frontend/src/app/GtwSeguridadProcesador/read/page.js
@@ -1,6 +1,8 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
+import "./page.css";
+
 
 const ViewPage = () => {
   const { id } = useParams(); // Obtiene el ID del registro de la URL
@@ -20,44 +22,21 @@ const ViewPage = () => {
   };
 
   return (
-    <main
-      style={{
-        maxWidth: "600px",
-        margin: "2rem auto",
-        padding: "2rem",
-        backgroundColor: "#f9fafb",
-        borderRadius: "8px",
-        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <h1 style={{ textAlign: "center", color: "#1e40af", marginBottom: "1.5rem" }}>
-        Visualizar Comisiones
-      </h1>
-      <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Visualizar Comisiones</h1>
+      <div className={styles.dataContainer}>
         {Object.entries(mockData).map(([key, value]) => (
           <div key={key}>
-            <strong style={{ color: "#1f2937" }}>{key.replace(/([A-Z])/g, " $1")}: </strong>
+            <strong className={styles.label}>
+              {key.replace(/([A-Z])/g, " $1")}:{" "}
+            </strong>
             <span>{value}</span>
           </div>
         ))}
       </div>
-
-      <button
-            type="button"
-            onClick={handleCancel}
-            style={{
-              backgroundColor: "#ef4444",
-              color: "#ffffff",
-              padding: "10px 20px",
-              fontSize: "1rem",
-              border: "none",
-              borderRadius: "4px",
-              cursor: "pointer",
-              marginTop: "1rem",
-            }}
-          >
-            Volver
-          </button>
+      <button type="button" onClick={handleCancel} className={styles.button}>
+        Volver
+      </button>
     </main>
   );
 };
