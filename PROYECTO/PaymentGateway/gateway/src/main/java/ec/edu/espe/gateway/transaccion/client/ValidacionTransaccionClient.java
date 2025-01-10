b42505e7a5e07cf75e0aba8bd4381c491f382360
mvespin1 <mvespin1@espe.edu.ec>
2025-01-09 22:28:18 -0500

    Actualización Comunicación Procesador de Pagos
    Actualización Comunicación Procesador de Pagos

--- PROYECTO/PaymentGateway/gateway/src/main/java/ec/edu/espe/gateway/transaccion/client/ValidacionTransaccionClient.java
+++ PROYECTO/PaymentGateway/gateway/src/main/java/ec/edu/espe/gateway/transaccion/client/ValidacionTransaccionClient.java
@@ -7,7 +7,7 @@ import ec.edu.espe.gateway.transaccion.model.ValidacionTransaccionDTO;
 import ec.edu.espe.gateway.transaccion.model.RespuestaValidacionDTO;
 import org.springframework.http.ResponseEntity;
 
-@FeignClient(name = "validacionTransaccion", url = "https://fb2e-192-188-58-23.ngrok-free.app")
+@FeignClient(name = "validacionTransaccion", url = "https://3.135.191.61")
 public interface ValidacionTransaccionClient {
     
     @PostMapping("/api/v1/transacciones")
